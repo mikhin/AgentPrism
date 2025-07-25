07202c2f09882cd43378a6a68db1fad8259b0b96
Yuri Mikhin <yuri@evilmartians.com>
2025-07-25 14:20:36 +0300

    Remove DetailsView and DetailsPage references to streamline routing and exports.

--- packages/sandbox/src/stores/router.ts
+++ packages/sandbox/src/stores/router.ts
@@ -1,7 +1,6 @@
 import { createRouter } from "@nanostores/router";
 
 import { BadgePage } from "../pages/BadgePage";
-import { DetailsPage } from "../pages/DetailsPage";
 import { HomePage } from "../pages/HomePage";
 import { SpanCardPage } from "../pages/SpanCardPage";
 import { SpanCardsListPage } from "../pages/SpanCardsListPage";
@@ -28,11 +27,6 @@ export const ROUTES = {
     path: "/tree-view",
     component: SpanCardsListPage,
     label: "Span Cards List",
-  },
-  details: {
-    path: "/details",
-    component: DetailsPage,
-    label: "Details",
   },
 } as const;
 
