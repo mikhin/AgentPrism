b42e1d21f33fa491a5bd903e1b49c6645e8cab2c
Ivan Eltsov <whanou97@gmail.com>
2025-08-20 17:16:25 +0400

    feat: add wrapping for span card content

--- packages/core/src/components/SpanCardContent.tsx
+++ packages/core/src/components/SpanCardContent.tsx
@@ -15,32 +15,26 @@ export const SpanCardContent = ({ data }: SpanCardContentProps) => {
   const Icon = getSpanCategoryIcon(data.type);
 
   return (
-    <div className="flex items-center">
+    <div className="flex flex-wrap items-center justify-start gap-1">
       <h3 className="mr-3 max-w-32 truncate text-sm leading-5 text-gray-900 dark:text-gray-200">
         {data.title}
       </h3>
 
-      <div className="flex items-center justify-start space-x-1">
-        <Badge
-          iconStart={<Icon className="size-2.5" />}
-          theme={getSpanCategoryTheme(data.type)}
-          size="xs"
-        >
-          {getSpanCategoryLabel(data.type)}
-        </Badge>
+      <Badge
+        iconStart={<Icon className="size-2.5" />}
+        theme={getSpanCategoryTheme(data.type)}
+        size="xs"
+      >
+        {getSpanCategoryLabel(data.type)}
+      </Badge>
 
-        <Badge
-          iconStart={<Coins className="size-2.5" />}
-          theme="gray"
-          size="xs"
-        >
-          {data.tokensCount}
-        </Badge>
+      <Badge iconStart={<Coins className="size-2.5" />} theme="gray" size="xs">
+        {data.tokensCount}
+      </Badge>
 
-        <Badge theme="gray" size="xs">
-          $ {data.cost}
-        </Badge>
-      </div>
+      <Badge theme="gray" size="xs">
+        $ {data.cost}
+      </Badge>
     </div>
   );
 };
